fd7f1674198ecac363249bedd88a4c07e496719b
Rakib-31 <37634514+Rakib-31@users.noreply.github.com>
2018-11-23 03:08:02 +0600

    A simple firebase project

---
+++ firebase-demo-project/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-demo-firebase-project';
+  courses$;
+
+  constructor(db: AngularFireDatabase){
+    this.courses$ = db.list('/courses');
+  }
+}
